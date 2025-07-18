2f908f2ed8dbf6dda4def159d481cf4fa9611366
Aravindh <aravind4271@gmail.com>
2025-07-18 09:33:23 +0530

    Aravindh change

--- src/main/java/LetCodePractice/Frames/SecondFrame.java
+++ src/main/java/LetCodePractice/Frames/SecondFrame.java
@@ -24,8 +24,6 @@ public class SecondFrame {
 
 //        driver.switchTo().frame(1);
 //        WebElement email = driver.findElement(By.xpath(""));
-//        email.sendKeys("aravindh123@gmail.com");
-//        driver.switchTo().defaultContent();
 
 
 
